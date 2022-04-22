3d7f906a8fe5d0c96956590c2a59f835f0f27b6f
Sean King <seanianking@gmail.com>
2022-04-22 15:46:54 -0600

    testing build via github pages

---
+++ build/precache-manifest.c5ac3e8391f4b618ad9132be11a13eef.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbde0120275096d443746c0f10334e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0705c6e10d224c3d0f6",
+    "url": "/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "c4fb6399a1ff3f971728",
+    "url": "/static/js/2.460489f6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.460489f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0705c6e10d224c3d0f6",
+    "url": "/static/js/main.f45f919d.chunk.js"
+  },
+  {
+    "revision": "5aff0f0dd227e5cfc66c",
+    "url": "/static/js/runtime-main.c979558c.js"
+  },
+  {
+    "revision": "7483cdb622437e70fb0ecc95f6b042c1",
+    "url": "/static/media/CodingResume.7483cdb6.pdf"
+  },
+  {
+    "revision": "45ec8ce02db11a71a978d6e39d57c099",
+    "url": "/static/media/ExpressionsLanding.45ec8ce0.png"
+  },
+  {
+    "revision": "a18bbcd56b44ce110c4b8b886a937e25",
+    "url": "/static/media/LandingPageImage.a18bbcd5.png"
+  },
+  {
+    "revision": "14c89a41e5154b4a2920df0115337ad5",
+    "url": "/static/media/React.14c89a41.png"
+  },
+  {
+    "revision": "f87139c43eb5e3616d43c4a00f8503aa",
+    "url": "/static/media/Sass.f87139c4.png"
+  },
+  {
+    "revision": "b58016c5258193f58e7b16f188a4ea73",
+    "url": "/static/media/adobe.b58016c5.svg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "6bf9b4da7da578b9d3447adc363d32e1",
+    "url": "/static/media/bulma-logo.6bf9b4da.png"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/static/media/css3.d3ad67fa.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/static/media/express.53051603.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/static/media/html5.24845c5c.svg"
+  },
+  {
+    "revision": "6ff17bd2dc7d786853da589cb373d553",
+    "url": "/static/media/jQuery.6ff17bd2.svg"
+  },
+  {
+    "revision": "bbcd4feefeba1d3d72b8dc1de48143d8",
+    "url": "/static/media/king239.bbcd4fee.JPG"
+  },
+  {
+    "revision": "07402800471de2e87f1a5444dec48c6e",
+    "url": "/static/media/landing area.07402800.jpg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/static/media/nodeJS.f434bfbb.svg"
+  },
+  {
+    "revision": "bdad1321d32a62f80711139186975c79",
+    "url": "/static/media/plannerLanding.bdad1321.png"
+  },
+  {
+    "revision": "9ad4e256dafe261685ce4d7dbb7170ec",
+    "url": "/static/media/searchPage.9ad4e256.jpg"
+  },
+  {
+    "revision": "cfab87fa9c021536d9c1fa1aa6025f9a",
+    "url": "/static/media/someBurgersEaten.cfab87fa.png"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/vanillaJS.87711407.svg"
+  }
+]);
